1dce6aae464ea35726b7e1918e7ac2e99d75c517
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-07-17 22:41:55 +0530

    feat: saved

--- DotnetApiDapper/DotnetApiDapper.Api/Controllers/PeopleController.cs
+++ DotnetApiDapper/DotnetApiDapper.Api/Controllers/PeopleController.cs
@@ -87,4 +87,65 @@ public class PeopleController : ControllerBase
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); // 500 internal server error + ex.message
         }
     }
+
+    [HttpPut("{id:int}")] // api/people/1 (PUT)
+    public async Task<IActionResult> UpdatePerson(int id,[FromBody]Person person)
+    {
+        try
+        {
+            if (id != person.Id)
+            {
+                return BadRequest("Id from querystring and body does not match"); //400
+            }
+            using IDbConnection connection = new SqlConnection(_connectionString);
+
+            string existingPersonSql = @"select Id from Person where Id = @id";
+
+            int? existringPersonId = await connection.QuerySingleOrDefaultAsync<int?>(existingPersonSql, new { id });
+
+            if (existringPersonId is null)
+            {
+                return NotFound($"person with id : {id} not found");
+            }
+
+            string sql = @"update Person 
+                            set FirstName=@FirstName,
+                            LastName=@LastName
+                            where Id=@Id
+                            ";
+            await connection.ExecuteAsync(sql, person);
+            return NoContent(); //204 No Content
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); // 500 internal server error + ex.message
+        }
+    }
+
+
+    [HttpDelete("{id:int}")] // api/people/1 (DELETE)
+    public async Task<IActionResult> DeletePerson(int id)
+    {
+        try
+        {
+            using IDbConnection connection = new SqlConnection(_connectionString);
+
+            string existingPersonSql = @"select Id from Person where Id = @id";
+
+            int? existringPersonId = await connection.QuerySingleOrDefaultAsync<int?>(existingPersonSql, new { id });
+
+            if (existringPersonId is null)
+            {
+                return NotFound($"person with id : {id} not found");
+            }
+
+            string sql = @"delete from Person where Id=@id";
+            await connection.ExecuteAsync(sql, new { id});
+            return NoContent(); //204 No Content
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); // 500 internal server error + ex.message
+        }
+    }
 }
